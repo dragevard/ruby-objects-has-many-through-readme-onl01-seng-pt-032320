2868ba88a6a2a32f0cb70898302bf5e64444cffe
Derek phung <dragevards@gmail.com>
2020-05-03 23:51:30 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,4 +1,18 @@
 class Customer
   
   attr_reader :name, :age 
+  
+  @@all = [] 
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal()
 end
